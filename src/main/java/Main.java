39e00765a190209faedc929faa3eccff00928458
Mark Tse <mark.tse@neverendingqs.com>
2015-12-27 19:03:12 -0500

    Got basic spark functionality running for 1.4.1.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,22 @@
+import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.SparkConf;
+
+import java.util.Arrays;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Test build and run");
+        System.out.println("Starting...");
+
+        SparkConf conf = new SparkConf().setAppName("sandbox").setMaster("local");
+        JavaSparkContext sc = new JavaSparkContext(conf);
+
+        List<Integer> data = Arrays.asList(1, 2, 3, 4, 5);
+        JavaRDD<Integer> distData = sc.parallelize(data);
+
+        int count = distData.reduce((a, b) -> a + b);
+
+        System.out.println(String.format("Count: %d", count));
     }
 }
